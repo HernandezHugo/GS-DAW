dbd1aafd4c8851458c8c8708ef921d68c8f8aded
hugo-clase <hugo.quimbay97@gmail.com>
2022-10-10 21:21:11 +0200

    js_kapreka

---
+++ DWEC/UD02/constante de Kaprekar/script.js
@@ -0,0 +1,16 @@
+const numero = document.getElementById("numero");
+const btn = document.getElementById("btn");
+const final_result = 6174;
+const test = [5, 6, 2, 3];
+const test2 = ["5", "6", "2", "3"];
+
+
+
+/* console.log(test.sort((a, b) => a - b));
+console.log(test.join(""));
+console.log(test.sort((a, b) => b - a));
+console.log(test2.sort((a, b) => a.localeCompare(b))); */
+
+btn.addEventListener("click", (e) => {
+    console.log(numero.innerText());
+});
