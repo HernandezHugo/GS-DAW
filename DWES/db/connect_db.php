51cc38bc2ef9bdc2c7b462b4521c39a4f2eb2f14
hugo-clase <hugo.quimbay97@gmail.com>
2022-10-28 16:55:10 +0200

    cambio de carpetas

--- DWES/db/connect_db.php
+++ DWES/db/connect_db.php
@@ -4,10 +4,10 @@
 
 //Connect to database
 $conn = mysqli_connect('localhost','root','','039_hotel');
+//$conn = mysqli_connect('dwesdatabase','dwess1234','test1234.','039_hotel');
 
 //check connection
 if(!$conn){
     echo 'Connection error: ' . mysqli_connect_error();
 };
-
 ?>
