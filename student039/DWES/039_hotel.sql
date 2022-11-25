887837ca7ccca1031364b3d8c14fc2a1915e160d
hugo-clase <hugo.quimbay97@gmail.com>
2022-11-25 19:01:22 +0100

    DIW - exam DWES - db

--- student039/DWES/039_hotel.sql
+++ student039/DWES/039_hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2022 at 07:32 PM
+-- Generation Time: Nov 24, 2022 at 09:17 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
