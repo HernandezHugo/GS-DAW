ff4299c3859195948ba0b169ed154a5ce7a2981b
hugo-clase <hugo.quimbay97@gmail.com>
2023-01-13 20:15:23 +0100

    diw jquery drag and drop

---
+++ DIW/UD04/activity 04/script.js
@@ -0,0 +1,31 @@
+$(".post").data("dropped", false)
+  .draggable({})
+  .on({
+    mouseenter: function () {
+      $(this).css("cursor", "grab");
+    },
+    mousedown: function () {
+      $(this).css("cursor", "grabbing");
+    },
+    mouseup: function () {
+      $(this).css("cursor", "grab");
+    },
+  });
+
+$(".drop").droppable({
+  accept: ".post",
+  drop: function (event, ui) {
+    let count = $(this).find("span").text();
+    $(this)
+      .find("span")
+      .text(+count + 1);
+      $(ui.draggable).data("dropped", true)
+    },
+    out: function (event, ui) {
+        let count = $(this).find("span").text();
+        $(this)
+        .find("span")
+        .text(+count - 1);
+        $(ui.draggable).data("dropped", false)
+  },
+});
