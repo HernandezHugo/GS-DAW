a02f768e0fa24c561fd2419b712caf80a0d56386
Hugo casa <hugo.quimbay97@gmail.com>
2023-01-19 06:19:28 +0100

    DIW - posts

--- DIW/UD04/activity 04/script.js
+++ DIW/UD04/activity 04/script.js
@@ -1,40 +1,96 @@
-$(".post")
-  .data("dropped", false)
-  .draggable({})
-  .on({
-    mouseenter: function () {
-      $(this).css("cursor", "grab");
-    },
-    mousedown: function () {
-      $(this).css("cursor", "grabbing");
-    },
-    mouseup: function () {
-      $(this).css("cursor", "grab");
-    },
-  });
-
 $(".drop").droppable({
-  accept: ".post",
   drop: function (event, ui) {
     let count = $(this).find("span").text();
-    $(this)
-      .find("span")
-      .text(+count + 1);
+    if ($(ui.draggable).data("dropped") !== true) count = +count + 1;
+    $(this).find("span").text(count);
     $(ui.draggable).data("dropped", true);
   },
   out: function (event, ui) {
     let count = $(this).find("span").text();
-    $(this)
-      .find("span")
-      .text(+count - 1);
+    if ($(ui.draggable).data("dropped") !== false) count = +count - 1;
+    $(this).find("span").text(count);
     $(ui.draggable).data("dropped", false);
   },
 });
 
-$.fn.createPostIt = function () {
-  $("<div><div/>", {
-    class: "post",
-  }).appendTo("posts");
-};
+$(".d-green").droppable({
+  accept: ".green",
+});
+$(".d-red").droppable({
+  accept: ".red",
+});
 
-$("add-green").on("click", createPostIt());
+function createPostIt(color, num) {
+  $(document.createElement("div"))
+    .attr({
+      class: "post " + color,
+    })
+    .append(
+      $(document.createElement("div"))
+        .attr({
+          class: "menu",
+        })
+        .append(
+          $(document.createElement("input")).attr({
+            type: "text",
+          }),
+          $(document.createElement("button"))
+            .attr({
+              class: "minimize",
+            })
+            .text("_"),
+          $(document.createElement("button"))
+            .attr({
+              class: "close",
+            })
+            .text("X")
+        )
+    )
+    .append(
+      $(document.createElement("textarea")).attr({
+        cols: 15,
+        rows: 6,
+      })
+    )
+    .data("prop", { dropped: false, id: num + 1 })
+    .draggable()
+    .on({
+      mouseenter: function () {
+        $(this).css("cursor", "grab");
+      },
+      mousedown: function () {
+        $(this).css("cursor", "grabbing");
+      },
+      mouseup: function () {
+        $(this).css("cursor", "grab");
+      },
+    })
+    .appendTo("#" + color);
+}
+
+function countPosts(color) {
+  return $("." + color).length;
+}
+
+$(".add-green").on({
+  click: function () {
+    createPostIt("green", countPosts("green"));
+  },
+});
+$(".add-red").on({
+  click: function () {
+    createPostIt("red", countPosts("red"));
+  },
+});
+
+/*to continue*/ 
+$(".minimize").on({
+  click: function () {
+    createPostIt("red");
+  },
+});
+$(".close").on({
+  click: function () {
+    createPostIt("red");
+  },
+});
