791fdbbb8e711ee7a9a1ad4e0caa2090455ff950
hugo-clase <hugo.quimbay97@gmail.com>
2023-01-24 18:10:40 +0100

    idk++

--- DIW/UD04/apispotify2/js/spotify-client.js
+++ DIW/UD04/apispotify2/js/spotify-client.js
@@ -1,36 +1,42 @@
-var client_id = '54daf2691cee41aab558f29651b04649';
-var client_secret = 'd21bd57ab1aa4529aae14522c474bad1';
-var access_token = '';
+var client_id = "54daf2691cee41aab558f29651b04649";
+var client_secret = "d21bd57ab1aa4529aae14522c474bad1";
+var access_token = "";
 
 //We create the Spotify class with the API to make the call to
 function Spotify() {
-  this.apiUrl = 'https://api.spotify.com/';
+  this.apiUrl = "https://api.spotify.com/";
+}
+
+//display artists
+function createArtistCard(artist) {
+  console.log(artist);
 }
 
 //Search for information on an artist, adding the possibility of obtaining their albums.
 Spotify.prototype.getArtist = function (artist) {
-
   $.ajax({
     type: "GET",
-    url: this.apiUrl + 'v1/search?type=artist&q=' + artist,
+    url: this.apiUrl + "v1/search?type=artist&q=" + artist,
     headers: {
-      'Authorization' : 'Bearer ' + access_token
+      Authorization: "Bearer " + access_token,
     },
-  }).done( function(response){
-    console.log(response);
+  }).done(function (response) {
+    $("#qty_results").find("span").text(response.artists.total);
+    $(response.artists.items).each(function (i, artist) {
+      createArtistCard(artist);
+    });
   });
 };
 
 //Search the albums of an artist, given the id of the artist
 Spotify.prototype.getArtistById = function (artistId) {
-
   $.ajax({
     type: "GET",
-    url: this.apiUrl + 'v1/artists/' + artistId + '/albums',
+    url: this.apiUrl + "v1/artists/" + artistId + "/albums",
     headers: {
-      'Authorization' : 'Bearer ' + access_token
+      Authorization: "Bearer " + access_token,
     },
-  }).done( function(response){
+  }).done(function (response) {
     console.log(response);
   });
 };
@@ -41,22 +47,24 @@ $(function () {
     type: "POST",
     url: "https://accounts.spotify.com/api/token",
     beforeSend: function (xhr) {
-      xhr.setRequestHeader ("Authorization", "Basic " + btoa(client_id + ":" + client_secret));
+      xhr.setRequestHeader(
+        "Authorization",
+        "Basic " + btoa(client_id + ":" + client_secret)
+      );
     },
     dataType: "json",
-    data: { grant_type: "client_credentials" }
-  }).done( function(response) {    
-    access_token = response.access_token;    
+    data: { grant_type: "client_credentials" },
+  }).done(function (response) {
+    access_token = response.access_token;
   });
 
   var spotify = new Spotify();
 
-  $('#bgetArtist').on('click', function () {
-    spotify.getArtist($('#artistName').val());
+  $("#bgetArtist").on("click", function () {
+    spotify.getArtist($("#artistName").val());
   });
 
-  $('#results').on('click', '.artistId', function () {
+  $("#results").on("click", ".artistId", function () {
     spotify.getArtistById($(this).attr("data-id"));
   });
-
 });
