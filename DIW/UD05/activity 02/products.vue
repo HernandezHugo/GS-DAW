fc1ebd989942f9e8fd52777b0eb32efc78851b68
hugo-clase <hugo.quimbay97@gmail.com>
2023-02-07 21:13:02 +0100

    diw vue-components

---
+++ DIW/UD05/activity 02/products.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      items: this.getData(),
+    };
+  },
+};
+</script>
+
+<template>
+  <h3>{{ item.product }}</h3>
+  <img v-bind:src="item.picture" />
+  <p>{{ item.price }}€</p>
+  <p>
+    <span v-for="tag in item.tags">#{{ tag }}</span>
+  </p>
+</template>
