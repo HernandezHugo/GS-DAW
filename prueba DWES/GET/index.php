b47c9b33c210a7778cb01f981a53018346f9b79a
hugo-clase <hugo.quimbay97@gmail.com>
2023-03-06 21:08:25 +0100

    dwes ajax - inicio

--- prueba DWES/GET/index.php
+++ prueba DWES/GET/index.php
@@ -1,36 +1,41 @@
 <html>
 
 <head>
-  <script>
-    function showHint(str) {
-      if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-      } else {
-        let URL = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
-        //let URL = "gethint.php";
-        let key = "XCDMFUeWVFh1hjgZSqPqsGzAHPG1VH38"
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("txtHint").innerHTML = this.responseText;
-          }
-        };
-        xhr.open("GET", URL + "?apikey=" + key + "&q=" + str, true);
-        xhr.send();
-      }
-    }
-  </script>
-</head>
 
-<body>
-  <p><b>Start typing a name in the input field below:</b></p>
-  <form action="">
-    <!-- <input type="date" min="2023-01-19"> -->
-    <label for="fname">First name:</label>
-    <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)" />
-  </form>
-  <p>Suggestions: <span id="txtHint"></span></p>
+<body onload="actionWeather()">
 </body>
+<script>
+  function actionWeather() {
+    let URL = "http://dataservice.accuweather.com/currentconditions/v1/";
+
+    let key = "XCDMFUeWVFh1hjgZSqPqsGzAHPG1VH38"
+    let locationkey = "305482"
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log("documento de tiempo adquirido");
+        saveWeather(this.responseText)
+      }
+    };
+    xhr.open("GET", URL + locationkey + "?apikey=" + key, true);
+    xhr.send();
+
+  }
+
+  function saveWeather(res) {
+
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log("documento de tiempo enviado");
+        console.log(res);
+      }
+    };
+    xhr.open("GET", "./pruebaGET.php?q=" + res, true)
+    xhr.send()
+  }
+</script>
+</head>
 
 </html>
